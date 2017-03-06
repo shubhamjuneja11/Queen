01c6fe1b43aa68c47a4af8f22ad2990e7e540b7d
shubhamjuneja11 <shubhamjuneja11@gmail.com>
2017-03-07 02:43:51 +0530

    Board prep

--- app/src/main/java/com/example/junejaspc/queen/ChessBoard.java
+++ app/src/main/java/com/example/junejaspc/queen/ChessBoard.java
@@ -1,5 +1,6 @@
 package com.example.junejaspc.queen;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -9,29 +10,54 @@ import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-public class ChessBoard extends AppCompatActivity {
+public class ChessBoard extends AppCompatActivity implements View.OnClickListener {
 GridLayout gridLayout;
-    Button button;
+    Button button,button_set[][];
     int i,height,width,totalbuttons,rowlimit;
     boolean j;
+    boolean decide;
     DisplayMetrics displayMetrics;
+    int buttons_state[][];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess_board);
         gridLayout=(GridLayout)findViewById(R.id.grid);
-        j=true;
         displayMetrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        j=true;
+        decide=true;
+        rowlimit=8;
+        buttons_state=new int[rowlimit][rowlimit];
         decideFactor();
     }
     public void decideFactor(){
         totalbuttons=rowlimit*rowlimit;
         height = displayMetrics.heightPixels/rowlimit;
         width = displayMetrics.widthPixels/rowlimit;
+        gridLayout.setRowCount(rowlimit);
+        gridLayout.setColumnCount(rowlimit);
         boardsetup();
     }
     public void boardsetup(){
+        int i,j;
+        button_set=new Button[rowlimit][rowlimit];
+        for(i=0;i<rowlimit;i++)
+        {
+            for(j=0;j<rowlimit;j++){
+               button_set[i][j]=new Button(this);
+                button_set[i][j].setLayoutParams(new LinearLayout.LayoutParams(width,width));
+                if(decide)
+                    button_set[i][j].setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                else  button_set[i][j].setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+                decide=!decide;
+                gridLayout.addView(button_set[i][j]);
+                button_set[i][j].setOnClickListener(this);
+            }
+            decide=!decide;
+        }
+    }
+    /*public void boardsetup(){
         for(i=0;i<totalbuttons;i++){
             if(i%rowlimit==0&&i>0)j=!j;
             button=new Button(this);
@@ -41,6 +67,13 @@ GridLayout gridLayout;
             else  button.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
             j=!j;
             gridLayout.addView(button);
+            button.setOnClickListener(this);
+
         }
+    }*/
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
